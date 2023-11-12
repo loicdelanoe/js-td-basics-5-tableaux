026ace4659bd45c9b8fd0afc1882da192cddfbb7
Loïc Delanoë <loic.del4127@gmail.com>
2023-11-12 16:08:42 +0100

    Finished tableaux-p4-date-valide.js

--- starter/tableaux-p4-date-valide.js
+++ starter/tableaux-p4-date-valide.js
@@ -10,11 +10,26 @@ TABLEAUX - PRÉPA 4 : Date valide - version 3
 // Fonction pour vérifier si une année est bissextile
 function isBissextile(annee) {
     // Code pour déterminer si 'annee' est bissextile
+    return (annee % 4 === 0 && !(annee % 100 === 0)) || annee % 400 === 0;
 }
 
 // Fonction pour vérifier si une date est valide
 function isValid(jour, mois, annee) {
     // Code pour vérifier si la date (jour, mois, annee) est valide
+    if (annee < 0) {
+        return false;
+    }
+    if (mois > 12 || mois < 0) {
+        return false;
+    }
+    if (jour < 0 || jour > 31) {
+        return false;
+    }
+
+    const joursParMois = [0, 31, isBissextile(annee) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    return jour <= joursParMois[mois];
 }
 
+console.log(isValid(25))
 // Utilisez 'isValid' pour vérifier si une date est valide avant d'afficher un message dans la console
